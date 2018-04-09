14a68c57a5b06ea0df2362e6c252ecbf24ce11bf
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-09 15:54:09 +0100

    first commit

---
+++ rspec-intro/rspec-intro.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+describe 'This is the description of the product you are going to test' do
+
+  context 'context is merely another way to break out your ocde and description' do
+
+    it 'the it block is where the tests begin and get run' do
+    # it 'it should return true' do 
+
+    end
+
+  end
+
+end
