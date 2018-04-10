cbfc8b575d6a4048bf2aa9e478a7f6b42dc6a3b7
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-10 11:05:16 +0100

    write test description

---
+++ rspec-intro/rspec_fizzbuzz/fizzBuzzOOP/spec/fizz_buzz_spec.rb
@@ -0,0 +1,19 @@
+require 'fizzbuzz'
+
+describe 'FizzBuzz' do
+
+  before(:each) do
+    @fb = FizzBuzz.new
+  end
+
+  it 'should respond true if the number is divisible by 3' do
+
+  end
+  it 'should respond true if the number is divisible by 5' do
+
+  end
+
+  it 'should correclt apply fizzbuzz to given range' do
+
+  end
+end
