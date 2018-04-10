2471ba4870ac4c4236bdbb538b12fc6fc5e2e384
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-10 11:58:17 +0100

    fizz and buzz to be inserted into its divisibles

--- rspec-intro/rspec_fizzbuzz/fizzBuzzOOP/spec/fizz_buzz_spec.rb
+++ rspec-intro/rspec_fizzbuzz/fizzBuzzOOP/spec/fizz_buzz_spec.rb
@@ -7,13 +7,29 @@ describe 'FizzBuzz' do
   end
 
   it 'should respond true if the number is divisible by 3' do
-    expect(@fb.divisible_by?(3)).to be true
+    expect(@fb.divisible_by?(9, 3)).to be true
   end
+
+  it 'should respond false if the number is cleanly visible by 3' do
+    expect(@fb.divisible_by?(5, 3)).to be false
+
+  end
+
   it 'should respond true if the number is divisible by 5' do
-    
+    expect(@fb.divisible_by?(10, 5)).to be true
   end
+
+  it 'should respond false if the number is cleanly visible by 5' do
+    expect(@fb.divisible_by?(6, 5)).to be false
+
+  end
+
 
   it 'should correclt apply fizzbuzz to given range' do
-
+    @fb.fizzbuzz_iterator(1, 100)
+    expect(@fb.fizzbuzz_array[0]).to eq 1
+    expect(@fb.fizzbuzz_array[14]).to eq 'FizzBuzz'
+    expect(@fb.fizzbuzz_array[2]).to eq 'Fizz'
+    expect(@fb.fizzbuzz_array[4]).to eq 'Buzz'
   end
 end
