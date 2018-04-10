1395c2908d5a3ffc55b6514cacd6575a4500196a
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-10 14:13:40 +0100

    first problem solved

---
+++ rspec-intro/rspec_project_euler/problem1/lib/projecteuler.rb
@@ -0,0 +1,29 @@
+class ProjectEuler
+
+  attr_accessor:projecteuler_array
+
+  def initialize
+    @projecteuler_array = []
+  end
+
+  def total
+    @projecteuler_array.sum
+  end
+
+  def divisible_by? (num, div_by_num)
+    (num % div_by_num).zero?
+  end
+
+  def projecteuler_iterator(range_from, range_to)
+    (range_from...range_to).each do |i|
+      if divisible_by?(i, 3)
+        @projecteuler_array << i
+      elsif divisible_by?(i, 5)
+        @projecteuler_array << i
+      end
+    end
+  end
+
+
+
+end
---
+++ rspec-intro/rspec_project_euler/problem1/spec/project_euler_spec.rb
@@ -0,0 +1,14 @@
+require 'projecteuler'
+
+describe 'ProjectEuler' do
+
+  before(:each) do
+    @pe = ProjectEuler.new
+  end
+
+  it 'should correclt sum up the total of multiples of 3 and 5 in range of 1000' do
+    @pe.projecteuler_iterator(1, 1000)
+    expect(@pe.total).to eq 233168
+  end
+
+end
