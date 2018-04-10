ac03b96f79743bd0b5d3dbc67a73b6307d2b2d73
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-10 09:57:28 +0100

    hook

--- rspec-intro/rspec_calculator/spec/calc_engine_spec.rb
+++ rspec-intro/rspec_calculator/spec/calc_engine_spec.rb
@@ -2,24 +2,29 @@ require 'spec_helper'
 
 describe 'Calcengine' do
 
+  # Hook to use this global variable to create a new test for calculation
+  before(:each) do
+    @calc = Calcengine.new
+  end
+
   it 'should correctly add two numbers' do
-    calc = Calcengine.new
-    expect(calc.add(1, 1)).to eql(2)
+    # calc = Calcengine.new
+    expect(@calc.add(1, 1)).to eql(2)
   end
 
   it 'should correctly subtract two numbers' do
-    calc = Calcengine.new
-    expect(calc.subtract(4, 1)).to eql(3)
+    # calc = Calcengine.new
+    expect(@calc.subtract(4, 1)).to eql(3)
   end
 
   it 'should correctly multiply two numbers' do
-    calc = Calcengine.new
-    expect(calc.multiply(1, 1)).to eql(1)
+    # calc = Calcengine.new
+    expect(@calc.multiply(1, 1)).to eql(1)
   end
 
   it 'should correctly divide two numbers' do
-    calc = Calcengine.new
-    expect(calc.divide(1, 1)).to eql(1)
+    # calc = Calcengine.new
+    expect(@calc.divide(1, 1)).to eql(1)
   end
 
 end
